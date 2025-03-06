818316f740d06a4c55393cd8ba4601d794acee37
batkt <168392649+batkt@users.noreply.github.com>
2025-03-06 12:21:00 +0800

    kill

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ pm2.connect(function (err) {
   // Clear logs every 10 minutes (600000 ms)
   setInterval(() => {
     console.log('Clearing PM2 logs...');
+
+    pm2.killDaemon((err) => {
+      if (err) {
+        console.error('Failed to kill PM2 logs:', err);
+      } else {
+        console.log('PM2 kill successfully.');
+      }
+    });
     
     pm2.flush((err) => {
       if (err) {
@@ -18,5 +26,16 @@ pm2.connect(function (err) {
         console.log('PM2 logs cleared successfully.');
       }
     });
-  }, 600000); // 10 minutes
+
+     // Starting the app with the ecosystem file
+    pm2.start('tureesShine/ecosystem.config.js', function (err, apps) {
+      if (err) {
+        console.error('Error while starting the apps:', err);
+        return pm2.disconnect(); // Disconnect PM2 if there's an error
+      }
+      console.log('PM2 apps have been started');
+      pm2.disconnect(); // Disconnect PM2 once done
+    });
+
+  }, 60000); // 10 minutes
 });
