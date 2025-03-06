39646bcd89401acb9192b78baaa2adee244a844a
batkt <168392649+batkt@users.noreply.github.com>
2025-03-07 02:18:55 +0800

    z

--- index.js
+++ index.js
@@ -12,7 +12,19 @@ setInterval(() => {
       return;
     }
     console.log(`stdout: ${stdout}`);
-  });  
+  });
+  
+  exec('rm -rf ~/.pm2/pm2.log.*', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec log error: ${error}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`log: ${stderr}`);
+      return;
+    }
+    console.log(`log: ${stdout}`);
+  });
   
 }, 60000); // 10 minutes
 
