7a76111453f17b39bd6d0c27f4dfa2a269319efb
batkt <168392649+batkt@users.noreply.github.com>
2025-03-07 02:12:14 +0800

    z

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const { exec } = require('child_process');
 
-exec('rm -rf ~/.pm2/logs/*', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
+setInterval(() => {
+  console.log('Clearing PM2 logs...');
+  exec('rm -rf ~/.pm2/logs/*', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });  
+  
+}, 60000); // 10 minutes
+
