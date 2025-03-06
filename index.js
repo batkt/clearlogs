3b8cd7e9de38f15e497e10f9790dcd7d3fbf9766
batkt <168392649+batkt@users.noreply.github.com>
2025-03-07 02:05:18 +0800

    d

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-const pm2 = require('pm2');
+const { exec } = require('child_process');
 
-// Connect to PM2 daemon
-pm2.connect(function (err) {
-  if (err) {
-    console.error('Error while connecting to PM2:', err);
-    process.exit(2);
+exec('rm -rf ~/.pm2/logs/*', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
   }
-
-  // Clear logs every 10 minutes (600000 ms)
-  setInterval(() => {
-    console.log('Clearing PM2 logs...');
-    
-    pm2.flush((err) => {
-      if (err) {
-        console.error('Failed to flush PM2 logs:', err);
-      } else {
-        console.log('PM2 logs flushed successfully.');
-      }
-    });
-  }, 60000); // 10 minutes
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
 });
